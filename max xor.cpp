02293310a6693b6dcb561adb2dc1080976f598ca
Aruna P <p.aruna1998@gmail.com>
2017-09-25 23:25:27 +0530

    Add files via upload

---
+++ max xor.cpp
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/maximizing-xor/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int l, r;
+    cin >> l >> r;
+    return 0;
+}
